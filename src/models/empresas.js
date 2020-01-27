49f024a6cb7b5325a801f588b9c8c4a3fac729c6
Matheus Santana de Sousa <matheussantana73@hotmail.com>
2020-01-27 00:49:27 -0300

    Datadbase de empresa rescrita para validar alguns campos

--- src/models/empresas.js
+++ src/models/empresas.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  name: String,
-  cnpj: String,
-  cidade: String,
-  telefone: String,
-  cep: String,
+  name: { type: String, required: true },
+  cnpj: { type: String, unique: true },
+  cidade: { type: String },
+  telefone: { type: String },
+  cep: { type: String },
 });
 const Empresa = mongoose.model('Empresa', schema);
 
