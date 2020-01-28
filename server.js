5bd47836469c90db6e4f53f097b4fbea8ea64aa7
Matheus Santana de Sousa <matheussantana73@hotmail.com>
2020-01-27 22:22:49 -0300

    Port da aplicacao para producao

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Config = require('./src/config/config');
 const setupApp = require('./src/app');
 
 setupApp()
-  .then((app) => app.listen(Config.PORT, () => console.info(`app running on port ${Config.PORT}`)))
+  .then((app) => app.listen(process.env.PORT || Config.PORT, () => console.info(`app running on port ${Config.PORT}`)))
   .catch((error) => {
     console.error(error);
     process.exit(1);
